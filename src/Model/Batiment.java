2cdbf05bf75706156be57b92b2f074bd65ed8b17
emmanuelJd <elisa.emmanuel.jd@gmail.com>
2013-11-16 01:38:46 +0100

    derniere modif

---
+++ src/Model/Batiment.java
@@ -0,0 +1,37 @@
+package modele;
+
+/**
+ * La classe Batiment, représente le batiment sur lequel évolue l'application, elle contient le nombre d'étage ainsi que les méthodes pour y accéder.
+ * 
+ * @author emmanuel
+ *
+ */
+public class Batiment {
+
+	private int nombre_etage;
+	
+	/**
+	 * 
+	 * @param nombre_etage lors de sa création on détermine le nombre d'étage que contient le batiment.
+	 */
+	public Batiment(int nombre_etage){
+		this.nombre_etage = nombre_etage;
+	}
+
+	/**
+	 * 
+	 * @return nombre_etage un entier qui est une variable de la classe et qui contient le nombre d'étage du batiment.
+	 */
+	public int getNombre_etage() {
+		return nombre_etage;
+	}
+
+	/**
+	 * 
+	 * @param nombre_etage un entier qui remplace le nombre d'étage du batiment.
+	 */
+	public void setNombre_etage(int nombre_etage) {
+		this.nombre_etage = nombre_etage;
+	}
+	
+}
